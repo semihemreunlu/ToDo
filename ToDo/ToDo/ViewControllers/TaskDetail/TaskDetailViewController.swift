07f28753604dc994cb97fc947dc08776741d132b
Semih Emre ÜNLÜ <semihemreunlu@gmail.com>
2020-10-04 22:33:54 +0300

    update taskDetailViewController ui

--- ToDo/ToDo/ViewControllers/TaskDetail/TaskDetailViewController.swift
+++ ToDo/ToDo/ViewControllers/TaskDetail/TaskDetailViewController.swift
@@ -9,10 +9,31 @@ import UIKit
 
 class TaskDetailViewController: BaseViewController<TaskDetailPresenter> {
     
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var noteTextField: UITextField!
+    @IBOutlet weak var notificationChoiceSwitch: UISwitch!
+    @IBOutlet weak var pickerDescriptionLabel: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var deleteButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         presenter?.load()
+        
+        saveButton.layer.cornerRadius = 8
+    }
+    
+    @IBAction func notificationChoiceSwitchStateChanged(_ sender: UISwitch) {
+        datePicker.isHidden = !sender.isOn
+        pickerDescriptionLabel.isHidden = !sender.isOn
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func deleteButtonTapped(_ sender: UIButton) {
     }
 }
 
