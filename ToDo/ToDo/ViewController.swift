9315fba4da2f4efc056d724549fb9ddd189f34e1
Semih Emre ÜNLÜ <semihemreunlu@gmail.com>
2020-10-04 17:18:07 +0300

    create xcode project

---
+++ ToDo/ToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Semih Emre Ünlü on 4.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
